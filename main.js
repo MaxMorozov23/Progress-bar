cae700b0de5a9bbf9d055b2683252c36526dc85b
MaxMorozov23 <31015359+MaxMorozov23@users.noreply.github.com>
2018-06-15 20:44:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myProgressBar").style.width = scrolled + "%";
+  document.getElementById("myProgressBar").innerHTML= Math.round(scrolled) + "%";
+}
